e84371ee7860f2b3297a2b2bf85be0306ceaa352
jayenliao <jay.chiehen@gmail.com>
2025-12-04 22:11:50 +0800

    refactor(ICL): explicitly handle the test sample

--- ICL.py
+++ ICL.py
@@ -1,11 +1,14 @@
 import argparse
 import torch
-import json
+import json, os
+from typing import Dict
+from models.basemodel import BaseModel
+
 parser = argparse.ArgumentParser(description="In-Context Learning (ICL) Configuration")
-parser.add_argument("--model_name", type=str, default="Qwen", choices=["Qwen", "Qwen2", "DeSTA2", "BLSP-Emo", "diva"], help="Name of the pre-trained language model to use.")
+parser.add_argument("--model_name", type=str, default="qwen", choices=["qwen", "qwen2", "desta2", "blsp-emo"], help="Name of the pre-trained language model to use.")
 parser.add_argument("--audio_task", type=str, default="ASR", help="The specific audio-related task.")
 parser.add_argument("--response_task", type=str, default="closed_ended_questions", help="The specific task for in-context learning.")
-parser.add_argument("--IF_task", type=str, default="change_case:english_capital", help="Whether to use in-context learning for the specified task.")
+parser.add_argument("--IF_task", type=str, default="change_case:english_capital", help="The format constraint task (i.e., instruction) for the model's response.")
 parser.add_argument("--examples", type=int, default=5, help="Number of in-context examples to use.")
 args = parser.parse_args()
 
@@ -17,27 +20,67 @@ IF_TASK = args.IF_task
 device = "cuda" if torch.cuda.is_available() else "cpu"
 AUDIOPATH = "./in-context-examples/audios/"
 
-from models.basemodel import BaseModel
+# TEST_EXAMPLE = {
+#     "audio_path": "./samples/sd-qa_1008642825401516622.wav",
+#     # "instruction": "what does the person in the first audio say?\nWrite everything in your response using capital letters only.",
+#     "instruction": "what does the person in the last audio say?\nWrite everything in your response using capital letters only.",       #   Test on the last audio example
+#     "answer": "None"
+# }
+
+TEST_EXAMPLE = {
+    "audio_path": "./in-context-examples/audios/general/11.wav",
+    # "instruction": "what does the person in the first audio say?\nWrite everything in your response using capital letters only.",
+    "instruction": "what does the person in the last audio say?\nWrite everything in your response using capital letters only.",       #   Test on the last audio example
+    "answer": ""
+}
+
+# "common_voice_en_31703154.mp3", "instruction": "Tell the gender of the speaker from this audio recording. Choose the answer from \"Man\" or \"Woman\""
+
+# TEST_EXAMPLE = {
+#     "audio_path": "./data/audios/Automatic_speech_recognition/61-70968-0011.flac",
+#     # "instruction": "what does the person in the first audio say?\nWrite everything in your response using capital letters only.",
+#     "instruction": "Now answer the question: what does the person in the last audio say?\nWrite everything in your response using capital letters only.",       #   Test on the last audio example
+#     "answer": ""
+# }
+
+
+# TEST_EXAMPLE = {
+#     "audio_path": "./data/audios/Gender_recognition/common_voice_en_31703154.mp3",
+#     # "instruction": "what does the person in the first audio say?\nWrite everything in your response using capital letters only.",
+#     "instruction": "Tell the gender of the speaker from this audio recording. Choose the answer from \"Man\" or \"Woman\"\nWrite everything in your response using capital letters only.",       #   Test on the last audio example
+#     "answer": ""
+# }
+
+
+TEST_EXAMPLE = {
+    "audio_path": "./data/audios/Gender_recognition/dia279_utt7.wav",
+    # "instruction": "what does the person in the first audio say?\nWrite everything in your response using capital letters only.",
+    "instruction": "Tell the gender of the speaker from this audio recording. Choose the answer from \"Man\" or \"Woman\"\nWrite everything in your response using capital letters only.",       #   Test on the last audio example
+    "answer": ""
+}
+
+
 def load_model(model_name) -> BaseModel:
-    match model_name:
-        case "Qwen":
+    match model_name.lower():
+        case "qwen":
             from models.Qwen import Qwen_Audio_Chat
             return Qwen_Audio_Chat(device=device)
-        case "Qwen2":   
+        case "qwen2":
             from models.Qwen2 import Qwen2_Audio_Chat
             return Qwen2_Audio_Chat(device=device)
-        case "DeSTA2":
+        case "desta2":
             from models.DeSTA2 import DeSTA2
             return DeSTA2(device=device)
-        case "BLSP-Emo":
+        case "blsp-emo":
             from models.blsp_emo import BLSP_Emo
             return BLSP_Emo(device=device)
+    raise ValueError(f"Model {model_name} not supported.")
 
-def GenerateICLExamples(data:list[dict]) -> list[dict]:
+def GenerateICLandTestExamples(icl_data:list[dict], test_example: Dict[str, str]) -> list[dict]:
     '''
-        Generate In-Context Learning Examples
+        Generate In-Context Learning Examples and concatenate with test example.
         Output format:
-            List[Dict] : 
+            List[Dict] :
             [
                 {
                     "audio_path": ...
@@ -52,20 +95,17 @@ def GenerateICLExamples(data:list[dict]) -> list[dict]:
             ]
     '''
     ret = []
-    for item in data:
+    for item in icl_data:
         ICL_example = {}
-        ICL_example["audio_path"] = AUDIOPATH+item["audio_path"]
+        ICL_example["audio_path"] = os.path.join(AUDIOPATH, item["audio_path"])
         ICL_example["instruction"] = item["instruction"]
-        ICL_example["answer"] = item.get("ans", None)
+        ICL_example["answer"] = f" [ANS] {item.get('ans', None)}"
         ret.append(ICL_example)
-    #   Insert test example 
-    test_example = {
-        "audio_path": "./samples/sd-qa_1008642825401516622.wav",
-        "instruction": "what does the person in the first audio say?\nWrite everything in your response using capital letters only.",
-        # "instruction": "what does the person in the last audio say?\nWrite everything in your response using capital letters only.",       #   Test on the last audio example
-        "answer": None
-    }
+        print("Added ICL example: ", ICL_example)
+
+    #   Insert test example at the end
     ret.append(test_example)
+
     return ret
 
 def main():
@@ -73,15 +113,20 @@ def main():
     with open(JSONPATH, "r") as f:
         InContextDataset = json.load(f)
     dataset = InContextDataset[AUDIO_TASK][RESPONSE_TASK][IF_TASK][:args.examples]
-    ICLexamples = GenerateICLExamples(dataset)
-    #   Load model 
+    ICLexamples = GenerateICLandTestExamples(dataset, TEST_EXAMPLE)
+    #   Load model
     model = load_model(MODELNAME)
-    print(f"{MODELNAME} model initialized.")
+    print(f"\033[93m{MODELNAME} model initialized.\033[0m")
     #   Process input
+    print()
+    print(ICLexamples)
     model.process_input(ICLexamples)
+    print("-- Input processed. ---")
+    print(f"\033[93m{model.messages}\033[0m")
+
     #   Generate response
     response = model.generate()
-    print("Model response : ", response)
+    print("Model response: ", f"\033[92m{response}\033[0m")
     return 0
 
 if __name__ == "__main__":

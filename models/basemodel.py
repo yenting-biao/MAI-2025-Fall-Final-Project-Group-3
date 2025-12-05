9278725f07e7b1c3770974fe8ca178d3e98652a9
jayenliao <jay.chiehen@gmail.com>
2025-12-05 22:10:07 +0800

    refactor(models): set messages attribute when init

--- models/basemodel.py
+++ models/basemodel.py
@@ -2,10 +2,10 @@
 class BaseModel:
     def __init__(self, model_name:str):
         self.model_name = model_name
+        self.messages = None
         pass
 
     def process_input(self, conversation:list[dict]):
-        self.messages = conversation
         raise NotImplementedError("Subclasses should implement this method")
 
     def generate(self) -> str:

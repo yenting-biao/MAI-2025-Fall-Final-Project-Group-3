c2e8e36bd953283fecde13306ac635a69de0d8d3
jayenliao <jay.chiehen@gmail.com>
2025-12-05 17:26:44 +0800

    style(qwen.sh): output_dir

--- scripts/qwen.sh
+++ scripts/qwen.sh
@@ -12,7 +12,7 @@ IF_TASKS=(
 EXAMPLES=(0 1 2 3 4 5 6 7 8)
 MODEL_NAMES=("qwen")
 SEEDS=(42)
-OUTPUT_DIR="outputs/jayliao_Lugia/"
+OUTPUT_DIR="model_responses/jayliao_lugia/"
 
 for model_name in "${MODEL_NAMES[@]}"; do
   for audio_task in "${AUDIO_TASKS[@]}"; do
@@ -20,7 +20,7 @@ for model_name in "${MODEL_NAMES[@]}"; do
       for IF_task in "${IF_TASKS[@]}"; do
         for examples in "${EXAMPLES[@]}"; do
           for seed in "${SEEDS[@]}"; do
-            python ICL.py \
+            python run.py \
               --model_name "${model_name}" \
               --audio_task "${audio_task}" \
               --response_task "${response_task}" \
@@ -34,5 +34,3 @@ for model_name in "${MODEL_NAMES[@]}"; do
     done
   done
 done
-
-

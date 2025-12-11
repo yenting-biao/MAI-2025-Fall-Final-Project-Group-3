21a5a10f0568ee5ad8e1ffd5bb060f2a726d64c3
jayenliao <jay.chiehen@gmail.com>
2025-12-11 23:14:44 +0800

    feat(eval_scripts): scripts for llm judement on qwen

---
+++ eval_scripts/qwen_cw.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MODEL_NAME="qwen"
+RESPONSE_TASK="closed_ended_questions"
+AUDIO_TASKS=("ASR" "SER" "GR")
+
+for audio_task in "${AUDIO_TASKS[@]}"; do
+    python -m eval_scripts.evaluation_main \
+        --model_name="${model_name}" \
+        --audio_task="${audio_task}" \
+        --response_task="${response_task}" \
+        --IF_task="${IF_task}" \
+        --examples="${examples}"
+done

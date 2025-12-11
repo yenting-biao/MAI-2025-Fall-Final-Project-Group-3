86b2463fcfa69d01e6a08a87d4fb8452a26aba26
jayenliao <jay.chiehen@gmail.com>
2025-12-11 23:20:00 +0800

    fix(eval_scripts): fn typo

--- eval_scripts/qwen_cw.sh
+++ eval_scripts/qwen_cw.sh
@@ -5,7 +5,7 @@ RESPONSE_TASK="closed_ended_questions"
 AUDIO_TASKS=("ASR" "SER" "GR")
 
 for audio_task in "${AUDIO_TASKS[@]}"; do
-    python -m eval_scripts.evaluation_main \
+    python -m eval_scripts.eval_llm_judge.py \
         --model_name="${model_name}" \
         --audio_task="${audio_task}" \
         --response_task="${response_task}" \

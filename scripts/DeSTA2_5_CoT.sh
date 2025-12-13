9ad0e4e87cd49b1a89a4532c36ae2e2917694407
tangchien <weitangchien@gmail.com>
2025-12-14 02:44:59 +0800

    DeSTA2_5 scripts

---
+++ scripts/DeSTA2_5_CoT.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=0
+export CUBLAS_WORKSPACE_CONFIG=:16:8
+export HF_HOME='./cache'
+
+AUDIO_TASKS=("ASR" "SER" "GR")
+RESPONSE_TASKS=("chain-of-thought")
+IF_TASKS=("chain-of-thought")
+EXAMPLES=(0 1 2 3 4 5 6 7 8)
+MODEL_NAMES=("desta2_5")
+SEEDS=(42)
+OUTPUT_DIR="${OUTPUT_DIR:-model_responses/}"
+
+for model_name in "${MODEL_NAMES[@]}"; do
+  for audio_task in "${AUDIO_TASKS[@]}"; do
+    for response_task in "${RESPONSE_TASKS[@]}"; do
+      for IF_task in "${IF_TASKS[@]}"; do
+        for examples in "${EXAMPLES[@]}"; do
+          for seed in "${SEEDS[@]}"; do
+            python run.py \
+              --model_name "${model_name}" \
+              --audio_task "${audio_task}" \
+              --response_task "${response_task}" \
+              --IF_task "${IF_task}" \
+              --seed "${seed}" \
+              --examples "${examples}" \
+              --output_dir "${OUTPUT_DIR}"
+          done
+        done
+      done
+    done
+  done
+done
---
+++ scripts/DeSTA2_5_cw.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+export CUDA_VISIBLE_DEVICES=0
+export CUBLAS_WORKSPACE_CONFIG=:16:8
+export HF_HOME='./cache'
+
+AUDIO_TASKS=("ASR" "SER" "GR")
+RESPONSE_TASKS=("creative_writing")
+IF_TASKS=(
+    "detectable_format:number_bullet_lists"
+    "keywords:existence"
+    "keywords:forbidden_words"
+    "length_constraints:number_words"
+    "length_constraints:number_sentences"
+    "length_constraints:number_paragraphs"
+)
+EXAMPLES=(0 1 2 3 4 5 6 7 8)
+MODEL_NAMES=("desta2_5")
+SEEDS=(42)
+OUTPUT_DIR="${OUTPUT_DIR:-model_responses/}"
+
+for model_name in "${MODEL_NAMES[@]}"; do
+  for audio_task in "${AUDIO_TASKS[@]}"; do
+    for response_task in "${RESPONSE_TASKS[@]}"; do
+      for IF_task in "${IF_TASKS[@]}"; do
+        for examples in "${EXAMPLES[@]}"; do
+          for seed in "${SEEDS[@]}"; do
+            python run.py \
+              --model_name "${model_name}" \
+              --audio_task "${audio_task}" \
+              --response_task "${response_task}" \
+              --IF_task "${IF_task}" \
+              --seed "${seed}" \
+              --examples "${examples}" \
+              --output_dir "${OUTPUT_DIR}"
+          done
+        done
+      done
+    done
+  done
+done

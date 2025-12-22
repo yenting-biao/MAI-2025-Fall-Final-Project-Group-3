d2528cd99da0c74c8c210709c9cc815fda26abb2
Biao <91837449+yenting-biao@users.noreply.github.com>
2025-12-22 13:58:23 +0000

    chore: add missing eval shell scripts

---
+++ eval_scripts/desta2_5_cot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run multiple
+MODEL_NAME="desta2_5"
+RESPONSE_TASKS=("chain-of-thought")
+AUDIO_TASKS=("ASR" "SER" "GR")
+
+for audio_task in "${AUDIO_TASKS[@]}"; do
+    for response_task in "${RESPONSE_TASKS[@]}"; do
+        python -m eval_scripts.eval_llm_judge \
+            --model_name="${MODEL_NAME}" \
+            --audio_task="${audio_task}" \
+            --response_task="${response_task}"
+    done
+done

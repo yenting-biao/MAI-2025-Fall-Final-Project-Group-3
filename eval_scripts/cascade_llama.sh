e2e773b1f77504967d5a4696de25e2c8c76e7e80
biao <91837449+yenting-biao@users.noreply.github.com>
2026-02-08 16:12:09 +0000

    Add cascade_llama-3_1-8b-instruct CoT IF judge result

---
+++ eval_scripts/cascade_llama.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+MODEL_NAME="cascade_Llama-3_1-8B-Instruct"
+RESPONSE_TASKS=("chain-of-thought")
+AUDIO_TASKS=("ASR" "SER" "GR" "MMAU")
+
+for audio_task in "${AUDIO_TASKS[@]}"; do
+    for response_task in "${RESPONSE_TASKS[@]}"; do
+        python -m eval_scripts.eval_llm_judge \
+            --model_name="${MODEL_NAME}" \
+            --audio_task="${audio_task}" \
+            --response_task="${response_task}"
+    done
+done

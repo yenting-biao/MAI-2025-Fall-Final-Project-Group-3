b97ae77519123460b5d4a648fa4863931cc25173
jayenliao <jay.chiehen@gmail.com>
2026-02-12 14:37:14 +0800

    style(scripts/): keep shell scripts for both w/ and w/o contraint instruction texts for qwen2

--- scripts/qwen2_ceq.sh
+++ scripts/qwen2_ceq.sh
@@ -18,7 +18,7 @@ IF_TASKS=(
 EXAMPLES=(0 1 2 3 4 5 6 7 8)
 MODEL_NAMES=("qwen2")
 SEEDS=(42)
-OUTPUT_DIR="${OUTPUT_DIR:-model_responses_no_constraints/}"
+OUTPUT_DIR="${OUTPUT_DIR:-model_responses/}"
 
 for model_name in "${MODEL_NAMES[@]}"; do
   for audio_task in "${AUDIO_TASKS[@]}"; do
@@ -33,8 +33,7 @@ for model_name in "${MODEL_NAMES[@]}"; do
               --IF_task "${IF_task}" \
               --seed "${seed}" \
               --examples "${examples}" \
-              --output_dir "${OUTPUT_DIR}" \
-              --no_output_constraints
+              --output_dir "${OUTPUT_DIR}"
           done
         done
       done
--- scripts/qwen2_ceq_no_constraints.sh
+++ scripts/qwen2_ceq_no_constraints.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 set -e
+
+export CUBLAS_WORKSPACE_CONFIG=:4096:8
 export CUBLAS_WORKSPACE_CONFIG=:16:8
 
-#   CoT
-AUDIO_TASKS=("MMAU")
-RESPONSE_TASKS=("chain-of-thought")
-IF_TASKS=("chain-of-thought")
+AUDIO_TASKS=("ASR" "SER" "GR" "MMAU")
+RESPONSE_TASKS=("closed_ended_questions")
+IF_TASKS=(
+    "change_case:english_capital"
+    "change_case:english_lowercase"
+    "detectable_format:json_format"
+    "startend:quotation"
+    "detectable_format:title"
+    "combination:repeat_prompt"
+    "startend:end_checker"
+)
 EXAMPLES=(0 1 2 3 4 5 6 7 8)
 MODEL_NAMES=("qwen2")
 SEEDS=(42)
-OUTPUT_DIR="${OUTPUT_DIR:-model_responses/}"    
+OUTPUT_DIR="${OUTPUT_DIR:-model_responses_no_constraints/}"
 
 for model_name in "${MODEL_NAMES[@]}"; do
   for audio_task in "${AUDIO_TASKS[@]}"; do
@@ -24,14 +33,11 @@ for model_name in "${MODEL_NAMES[@]}"; do
               --IF_task "${IF_task}" \
               --seed "${seed}" \
               --examples "${examples}" \
-              --output_dir "${OUTPUT_DIR}"
+              --output_dir "${OUTPUT_DIR}" \
+              --no_output_constraints
           done
         done
       done
     done
   done
 done
-
-
-
-
